42c4b1b6e32dd762e99e81d862fcfa3ba78e12af
ELRNZgprc <richejohn@126.com>
2020-05-28 19:29:05 +0800

    leetcode 111,704,2,3

--- src/leetcode/Run.java
+++ src/leetcode/Run.java
@@ -4,29 +4,7 @@ package leetcode;
 public class Run {
     public static void main(String args[]) {
         Solution solution = new Solution();
-        char[][] board = new char[][]{
-                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
-                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
-        };
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-        solution.solveSudoku(board);
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
+        String s = "pwwkew";
+        System.out.println(solution.lengthOfLongestSubstring(s));
     }
 }
--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
     /**
      * Mianshiti 60
@@ -149,6 +152,119 @@ public class Solution {
             }
         }
         return false;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 111
+     *
+     * @param root TreeNode
+     * @return int
+     */
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right != null) {
+            return 1 + minDepth(root.right);
+        }
+        if (root.right == null && root.left != null) {
+            return 1 + minDepth(root.left);
+        }
+
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+
+    /**
+     * 704
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int half = (i + j) / 2;
+            if (nums[half] == target) {
+                return half;
+            } else if (nums[half] > target) {
+                j = half - 1;
+            } else {
+                i = half + 1;
+            }
+        }
+        return -1;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 2
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode p = root;
+        int flag = 0;
+        while ((l1 != null) || (l2 != null) || (flag > 0)) {
+            int ans = ((l1 != null) ? l1.val : 0) + ((l2 != null) ? l2.val : 0) + flag;
+            flag = ans / 10;
+            ans = ans % 10;
+            ListNode q = new ListNode(ans);
+            p.next = q;
+            p = q;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return root.next;
+    }
+
+    /**
+     * 3
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+        int[] hash = new int[128];
+        for(int i= 0;i<128;i++){
+            hash[i]=-1;
+        }
+        int start= 0;
+        for (int i = 0;i<s.length();i++){
+            int num = s.charAt(i);
+            start = Math.max(start,hash[num]+1);
+            ans = Math.max(ans,i-start+1);
+            hash[num] = i;
+        }
+        return ans;
     }
 }
 
