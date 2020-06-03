916abe5fdfb55b2f85d544d01007359abeb04325
ELRNZgprc <richejohn@126.com>
2020-06-03 19:51:02 +0800

    Leetcode 837

--- src/leetcode/Run.java
+++ src/leetcode/Run.java
@@ -3,6 +3,6 @@ package leetcode;
 public class Run {
     public static void main(String args[]) {
         Solution solution = new Solution();
-        System.out.println(solution.minAddToMakeValid(""));
+        System.out.println(solution.new21Game(9811,8890,7719));
     }
 }
--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -667,6 +667,41 @@ public class Solution {
         ans += left;
         return ans;
     }
+
+    /**
+     * 837
+     * https://leetcode-cn.com/problems/new-21-game/
+     *
+     * @param N
+     * @param K
+     * @param W
+     * @return
+     */
+    public double new21Game(int N, int K, int W) {
+        double[] dp = new double[K + W];
+        dp[0] = 1.0;
+        double pos = 0;
+        for (int i = 1; i <= K + W - 1; i++) {
+            if (i - W - 1 >= 0) {
+                pos -= dp[i - W - 1];
+            }
+            if (i - 1 < K) {
+                pos += dp[i - 1];
+            }
+            dp[i] = pos * 1.0 / W;
+        }
+        double sum = 0;
+        double usable = 0;
+        for (int i = K; i <= N; i++) {
+            if (i <= K + W - 1) {
+                usable += dp[i];
+            }
+        }
+        for (int i = K; i <= K + W - 1; i++) {
+            sum += dp[i];
+        }
+        return usable / sum;
+    }
 }
 
 
