2e06d91ccb8dad3bd015904b6733299a896261f3
ELRNZgprc <richejohn@126.com>
2020-07-30 00:59:57 +0800

    343

--- src/leetcode/Run.java
+++ src/leetcode/Run.java
@@ -1,7 +1,7 @@
 package leetcode;
 
 public class Run {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Solution solution = new Solution();
 
         System.out.println(solution.integerBreak(10));
--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -1200,16 +1200,16 @@ public class Solution {
      * @return
      */
     public int integerBreak(int n) {
-        if (n==2) return 1;
-        if (n==3) return 2;
-        int[] ans = new int[n+1];
-        ans[0]=0;
-        ans[1]=1;
-        ans[2]=2;
-        ans[3]=3;
-        for (int i=4;i<=n;i++){
-            for (int j =1;j<i;j++){
-                ans[i]=Math.max(ans[i],ans[j]*(i-j));
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int[] ans = new int[n + 1];
+        ans[0] = 0;
+        ans[1] = 1;
+        ans[2] = 2;
+        ans[3] = 3;
+        for (int i = 4; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                ans[i] = Math.max(ans[i], ans[j] * (i - j));
             }
         }
         return ans[n];
