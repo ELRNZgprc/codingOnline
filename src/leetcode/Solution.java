325519fef8c4978a5d194fcbf5b285a4fd7c3695
ELRNZgprc <richejohn@126.com>
2020-08-16 15:25:28 +0800

    0816

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -4045,4 +4045,46 @@ public class Solution {
         }
         return sb.toString();
     }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        return dfs(image, sr, sc, newColor, image[sr][sc]);
+    }
+
+    /**
+     * 733
+     * https://leetcode-cn.com/problems/flood-fill/
+     */
+    public int[][] dfs(int[][] image, int i, int j, int newColor, int num) {
+        if (i < 0 || i >= image.length || j < 0 || j >= image[0].length || image[i][j] == newColor || image[i][j] != num) {
+
+        } else {
+            int temp = image[i][j];
+            image[i][j] = newColor;
+            dfs(image, i + 1, j, newColor, temp);
+            dfs(image, i - 1, j, newColor, temp);
+            dfs(image, i, j + 1, newColor, temp);
+            dfs(image, i, j - 1, newColor, temp);
+
+        }
+        return image;
+    }
+
+    /**
+     * 434
+     * https://leetcode-cn.com/problems/number-of-segments-in-a-string/
+     */
+    public int countSegments(String s) {
+        if (s.equals("")) {
+            return 0;
+        }
+        String[] arr = s.split(" ");
+        int len = 0;
+        for (String t : arr) {
+            if (!(t.equals(" ") || t.isEmpty())) {
+                len++;
+            }
+
+        }
+        return len;
+    }
 }
