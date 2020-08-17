a70cead7ac3bed79ccbeef5265dba6f640c84b71
ELRNZgprc <richejohn@126.com>
2020-08-17 19:30:43 +0800

    添加 518. 零钱兑换 II

---
+++ src/leetcode/problem/change.java
@@ -0,0 +1,20 @@
+package leetcode.problem;
+
+public class change {
+    /**
+     * 518
+     * https://leetcode-cn.com/problems/coin-change-2/
+     */
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = 1; i < amount + 1; i++) {
+                if (i - coin >= 0) {
+                    dp[i] += dp[i - coin];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
