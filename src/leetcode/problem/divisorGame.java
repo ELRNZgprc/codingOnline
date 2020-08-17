ed89194ae301756ece25ec743362dbf7394fe044
ELRNZgprc <richejohn@126.com>
2020-08-17 18:56:30 +0800

    添加 1025. 除数博弈

---
+++ src/leetcode/problem/divisorGame.java
@@ -0,0 +1,11 @@
+package leetcode.problem;
+
+public class divisorGame {
+    /**
+     * 1025
+     * https://leetcode-cn.com/problems/divisor-game/
+     */
+    public boolean divisorGame(int N) {
+        return (N % 2 == 0);
+    }
+}
