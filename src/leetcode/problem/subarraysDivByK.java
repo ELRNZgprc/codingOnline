556a9f0e63a5e68d2d7dbc067d5f50252776c6ae
ELRNZgprc <richejohn@126.com>
2020-08-17 15:48:43 +0800

    添加 974. 和可被 K 整除的子数组

---
+++ src/leetcode/problem/subarraysDivByK.java
@@ -0,0 +1,26 @@
+package leetcode.problem;
+
+public class subarraysDivByK {
+    /**
+     * 974
+     * https://leetcode-cn.com/problems/subarray-sums-divisible-by-k/
+     */
+    public int subarraysDivByK(int[] A, int K) {
+        int[] hash = new int[K];
+        int sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum = (sum + A[i]) % K;
+            if (sum < 0)
+                sum += K;
+            hash[sum]++;
+        }
+
+        int ans = hash[0];
+        for (int i = 0; i < K; i++) {
+            if (hash[i] >= 2) {
+                ans += (hash[i] * (hash[i] - 1)) / 2;
+            }
+        }
+        return ans;
+    }
+}
