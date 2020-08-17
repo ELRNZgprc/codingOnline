08e3b888d8e93d2e4e91e8e9f35fac27d4d6768d
ELRNZgprc <richejohn@126.com>
2020-08-17 20:02:30 +0800

    添加 21. 合并两个有序链表

---
+++ src/leetcode/problem/mergeTwoLists.java
@@ -0,0 +1,24 @@
+package leetcode.problem;
+
+import leetcode.common.ListNode;
+
+public class mergeTwoLists {
+    /**
+     * 21
+     * https://leetcode-cn.com/problems/merge-two-sorted-lists/
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode head = null;
+        if (l1.val <= l2.val) {
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
+}
