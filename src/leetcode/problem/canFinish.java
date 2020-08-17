872a70f4f4ff04b3b2d7b1a15683c8c18fefe47f
ELRNZgprc <richejohn@126.com>
2020-08-17 16:56:38 +0800

    添加 207. 课程表

---
+++ src/leetcode/problem/canFinish.java
@@ -0,0 +1,40 @@
+package leetcode.problem;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class canFinish {
+    /**
+     * 207
+     * https://leetcode-cn.com/problems/course-schedule/
+     */
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] counts = new int[numCourses];
+        Queue<Integer> safety = new LinkedList<>();
+        for (int[] i : prerequisites) {
+            counts[i[0]]++;
+        }
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] == 0) {
+                safety.offer(i);
+            }
+        }
+
+        while (safety.size() != 0) {
+            int poll = safety.poll();
+            for (int[] i : prerequisites) {
+                if (i[1] == poll) {
+                    counts[i[0]]--;
+                    if (counts[i[0]] == 0) {
+                        safety.offer(i[0]);
+                    }
+                }
+            }
+        }
+
+        for (int c : counts) {
+            if (c != 0) return false;
+        }
+        return true;
+    }
+}
