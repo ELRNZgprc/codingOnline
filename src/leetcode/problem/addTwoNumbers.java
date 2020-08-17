964970dff8b1ca7559c87d540da18fa8ab23b21d
ELRNZgprc <richejohn@126.com>
2020-08-17 15:41:26 +0800

    添加 2. 两数相加

---
+++ src/leetcode/problem/addTwoNumbers.java
@@ -0,0 +1,30 @@
+package leetcode.problem;
+
+import leetcode.common.ListNode;
+
+public class addTwoNumbers {
+    /**
+     * 2
+     * https://leetcode-cn.com/problems/add-two-numbers/
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode p = root;
+        int flag = 0;
+        while ((l1 != null) || (l2 != null) || (flag > 0)) {
+            int ans = ((l1 != null) ? l1.val : 0) + ((l2 != null) ? l2.val : 0) + flag;
+            flag = ans / 10;
+            ans = ans % 10;
+            ListNode q = new ListNode(ans);
+            p.next = q;
+            p = q;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return root.next;
+    }
+}
