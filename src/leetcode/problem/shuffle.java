fc92da2149c35337dede7ccd4bcb4a2ff28ad2b9
ELRNZgprc <richejohn@126.com>
2020-08-17 19:07:27 +0800

    添加 1470. 重新排列数组

---
+++ src/leetcode/problem/shuffle.java
@@ -0,0 +1,20 @@
+package leetcode.problem;
+
+public class shuffle {
+    /**
+     * 1470
+     * https://leetcode-cn.com/problems/shuffle-the-array/
+     */
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2 * n];
+
+        for (int i = 0; i < 2 * n; i++) {
+            if (i % 2 == 0) {
+                ans[i] = nums[i / 2];
+            } else {
+                ans[i] = nums[n + i / 2];
+            }
+        }
+        return ans;
+    }
+}
