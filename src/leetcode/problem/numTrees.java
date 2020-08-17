a1a6aea34e841ccfecde0c9d472ef88e13fcf1a6
ELRNZgprc <richejohn@126.com>
2020-08-17 19:05:59 +0800

    添加 96. 不同的二叉搜索树

---
+++ src/leetcode/problem/numTrees.java
@@ -0,0 +1,22 @@
+package leetcode.problem;
+
+public class numTrees {
+    /**
+     * 96
+     * https://leetcode-cn.com/problems/unique-binary-search-trees/
+     */
+    public int numTrees(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
+}
