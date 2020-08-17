08b4d515d1b87927d7f535eb94ee84f860d0189f
ELRNZgprc <richejohn@126.com>
2020-08-17 20:16:00 +0800

    添加 1063. 有效子数组的数目

---
+++ src/leetcode/problem/validSubarrays.java
@@ -0,0 +1,25 @@
+package leetcode.problem;
+
+public class validSubarrays {
+    /**
+     * 1063
+     * https://leetcode-cn.com/problems/number-of-valid-subarrays/
+     */
+    public int validSubarrays(int[] nums) {
+        if (nums.length < 1) {
+            return 0;
+        }
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans++;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] >= nums[i]) {
+                    ans++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
