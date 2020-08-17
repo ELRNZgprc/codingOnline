091d01ff7d4704ae6c79e493e7837e58dc86e603
ELRNZgprc <richejohn@126.com>
2020-08-17 19:55:11 +0800

    添加 263. 丑数

---
+++ src/leetcode/problem/isUgly.java
@@ -0,0 +1,26 @@
+package leetcode.problem;
+
+public class isUgly {
+    /**
+     * 263
+     * https://leetcode-cn.com/problems/ugly-number/
+     */
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        if (num < 0) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+}
