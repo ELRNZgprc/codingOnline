4012c0a5faa0cc884b0e531be960aa89df0ed4bd
ELRNZgprc <richejohn@126.com>
2020-08-20 10:23:34 +0800

    添加 86. 分隔链表

---
+++ src/leetcode/problem/Partition.java
@@ -0,0 +1,31 @@
+package leetcode.problem;
+
+import leetcode.common.ListNode;
+
+public class Partition {
+    /**
+     * 86. 分隔链表
+     * https://leetcode-cn.com/problems/partition-list/
+     */
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummyHead1 = new ListNode(0);
+        ListNode dummyHead2 = new ListNode(0);
+        ListNode node1 = dummyHead1;
+        ListNode node2 = dummyHead2;
+        while (head != null) {
+            if (head.val < x) {
+                node1.next = head;
+                head = head.next;
+                node1 = node1.next;
+                node1.next = null;
+            } else {
+                node2.next = head;
+                head = head.next;
+                node2 = node2.next;
+                node2.next = null;
+            }
+        }
+        node1.next = dummyHead2.next;
+        return dummyHead1.next;
+    }
+}
