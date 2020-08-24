b3dc9b38653d45ad800778d623f4f98368d87b84
ELRNZgprc <richejohn@126.com>
2020-08-24 16:47:07 +0800

    添加 剑指 Offer 42. 连续子数组的最大和

--- src/leetcode/offer/MaxSubArray.java
+++ src/leetcode/offer/MaxSubArray.java
@@ -2,6 +2,7 @@ package leetcode.offer;
 
 public class MaxSubArray {
     /**
+     * 剑指 Offer 42. 连续子数组的最大和
      * https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
      */
     public int maxSubArray(int[] nums) {
