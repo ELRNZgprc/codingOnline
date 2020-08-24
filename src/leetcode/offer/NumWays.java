24b2a7abf30c5234a469720bc5f39fd3157c1ff3
ELRNZgprc <richejohn@126.com>
2020-08-24 19:20:42 +0800

    添加 剑指 Offer 10- II. 青蛙跳台阶问题

--- src/leetcode/offer/NumWays.java
+++ src/leetcode/offer/NumWays.java
@@ -6,15 +6,15 @@ public class NumWays {
      * https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/
      */
     public int numWays(int n) {
-        if (n==0) return 1;
-        if (n==1) return 1;
-        int a= 1;
-        int b =1;
-        int ans =2;
-        for (int i = 3;i<=n;i++){
-            b=a;
-            a=ans;
-            ans = (a+b)%(1000000007);
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        int a = 1;
+        int b = 1;
+        int ans = 2;
+        for (int i = 3; i <= n; i++) {
+            b = a;
+            a = ans;
+            ans = (a + b) % (1000000007);
         }
         return ans;
     }
