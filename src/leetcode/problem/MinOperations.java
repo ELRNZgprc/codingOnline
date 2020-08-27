c48b55521b25777a87d43bee131be5c8f5847498
ELRNZgprc <richejohn@126.com>
2020-08-27 18:22:31 +0800

    添加 1551. 使数组中所有元素相等的最小操作数

---
+++ src/leetcode/problem/MinOperations.java
@@ -0,0 +1,11 @@
+package leetcode.problem;
+
+public class MinOperations {
+    /**
+     * 1551. 使数组中所有元素相等的最小操作数
+     * https://leetcode-cn.com/problems/minimum-operations-to-make-array-equal/
+     */
+    public int minOperations(int n) {
+        return n * n / 4;
+    }
+}
