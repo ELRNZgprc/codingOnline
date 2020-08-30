ac7f93eb9950e51bbe59c158a456c9e339364a43
ELRNZgprc <richejohn@126.com>
2020-08-30 16:11:09 +0800

    添加 557. 反转字符串中的单词 III

---
+++ src/leetcode/problem/ReverseWords.java
@@ -0,0 +1,17 @@
+package leetcode.problem;
+
+public class ReverseWords {
+    /**
+     * 557. 反转字符串中的单词 III
+     * https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/
+     */
+    public String reverseWords(String s) {
+        String[] strs = s.split(" ");
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < strs.length; i++) {
+            buffer.append(new StringBuffer(strs[i]).reverse().toString());
+            buffer.append(" ");
+        }
+        return buffer.toString().trim();
+    }
+}
