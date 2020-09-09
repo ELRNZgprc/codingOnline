8bf178b4399228e738cf603ed3832f32c17e93d9
ELRNZgprc <richejohn@126.com>
2020-09-09 09:32:11 +0800

    添加 334. 递增的三元子序列

---
+++ src/leetcode/problem/IncreasingTriplet.java
@@ -0,0 +1,23 @@
+package leetcode.problem;
+
+public class IncreasingTriplet {
+    /**
+     * 334. 递增的三元子序列
+     * https://leetcode-cn.com/problems/increasing-triplet-subsequence/
+     */
+    public boolean increasingTriplet(int[] nums) {
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= a) {
+                a = num;
+            } else if (num <= b) {
+                b = num;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
