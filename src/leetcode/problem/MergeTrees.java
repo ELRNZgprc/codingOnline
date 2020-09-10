c028af6a93c8fa77c81edd49c3103e59f3112792
ELRNZgprc <richejohn@126.com>
2020-09-10 16:31:31 +0800

    添加 617. 合并二叉树

---
+++ src/leetcode/problem/MergeTrees.java
@@ -0,0 +1,22 @@
+package leetcode.problem;
+
+import leetcode.common.TreeNode;
+
+public class MergeTrees {
+    /**
+     * 617. 合并二叉树
+     * https://leetcode-cn.com/problems/merge-two-binary-trees/
+     */
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if ((t1 == null) &&(t2==null)){
+            return null;
+        }
+        if ((t1 == null)||(t2==null)){
+            return (t1==null)?t2:t1;
+        }
+        TreeNode t3 = new TreeNode(t1.val+t2.val);
+        t3.left = mergeTrees(t1.left,t2.left);
+        t3.right = mergeTrees(t1.right,t2.right);
+        return t3;
+    }
+}
