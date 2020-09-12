b772d3fba3828c1557deab52625f54d69d490862
ELRNZgprc <richejohn@126.com>
2020-09-12 19:19:30 +0800

    添加 1524. 和为奇数的子数组数目

--- src/leetcode/problem/NumOfSubarrays.java
+++ src/leetcode/problem/NumOfSubarrays.java
@@ -6,18 +6,19 @@ public class NumOfSubarrays {
      * https://leetcode-cn.com/problems/number-of-sub-arrays-with-odd-sum/
      */
     public int numOfSubarrays(int[] arr) {
-        int[][] dp = new int[arr.length][2];
-        dp[0][arr[0] % 2]++;
-        int ans = dp[0][1];
-        for (int i = 1; i < arr.length; i++) {
+        int odd = 0;
+        int even = 0;
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 1) {
-                dp[i][1] = dp[i - 1][0] + 1;
-                dp[i][0] = dp[i - 1][1];
+                int k = 0;
+                k = odd;
+                odd = even + 1;
+                even = k;
             } else {
-                dp[i][1] = dp[i - 1][1];
-                dp[i][0] = dp[i - 1][0] + 1;
+                even++;
             }
-            ans = (ans + dp[i][1]) % 1000000007;
+            ans = (ans + odd) % 1000000007;
         }
         return ans;
     }
