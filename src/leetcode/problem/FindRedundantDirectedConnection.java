2b128ea02e1a71caa8dce0b28d84796f562e16ab
ELRNZgprc <richejohn@126.com>
2020-09-17 09:25:15 +0800

    添加 685. 冗余连接 II

---
+++ src/leetcode/problem/FindRedundantDirectedConnection.java
@@ -0,0 +1,74 @@
+package leetcode.problem;
+
+public class FindRedundantDirectedConnection {
+    /**
+     * 685. 冗余连接 II
+     * https://leetcode-cn.com/problems/redundant-connection-ii/
+     */
+    static class UnionFind {
+        int[] parent;
+        int[] rank;
+
+        UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < parent.length; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public boolean union(int a, int b) {
+            int pa = find(a);
+            int pb = find(b);
+            if (pa == pb) return false;
+            if (rank[pa] < rank[pb]) {
+                parent[pa] = pb;
+            } else {
+                parent[pb] = pa;
+                if (rank[pa] == rank[pb]) {
+                    rank[pa]++;
+                }
+            }
+            return true;
+        }
+
+        int find(int a) {
+            while (a != parent[a]) {
+                parent[a] = parent[parent[a]];
+                a = parent[a];
+            }
+            return a;
+        }
+    }
+
+    public int[] findRedundantDirectedConnection(int[][] edges) {
+        if (edges == null || edges.length == 0) {
+            return null;
+        }
+        int[] c1 = new int[2];
+        int[] c2 = new int[2];
+
+        int[] parent = new int[edges.length + 1];
+        for (int[] edge : edges) {
+            if (parent[edge[1]] != 0) {
+                c1 = new int[]{parent[edge[1]], edge[1]};
+                c2 = new int[]{edge[0], edge[1]};
+                break;
+            }
+            parent[edge[1]] = edge[0];
+        }
+        UnionFind uf = new UnionFind(edges.length + 1);
+        for (int[] e : edges) {
+            if ((e[0] == c1[0] && e[1] == c1[1]) || (e[0] == c2[0] && e[1] == c2[1])) {
+                continue;
+            }
+            if (!uf.union(e[0], e[1])) {
+                return e;
+            }
+        }
+        if (!uf.union(c1[0], c1[1])) {
+            return c1;
+        }
+        return c2;
+    }
+}
