c6161b9732021d335ddadb0220495e144288a68d
ELRNZgprc <richejohn@126.com>
2020-09-18 08:58:05 +0800

    添加 LCP 07. 传递信息

--- src/leetcode/lcp/NumWays.java
+++ src/leetcode/lcp/NumWays.java
@@ -1,5 +1,28 @@
 package leetcode.lcp;
 
 public class NumWays {
+	/**
+	 * LCP 07. ´«µÝÐÅÏ¢
+	 * https://leetcode-cn.com/problems/chuan-di-xin-xi/
+	 */
+	private int plans = 0;
 
+    public int numWays(int n, int[][] relation, int k) {
+    	pass(0, 1, n, relation, k);
+    	return plans;
+    }   
+
+    public void pass(int start, int times, int n, int[][] relation, int k) {
+    	for (int i = 0; i < relation.length; i++) {
+			if (relation[i][0] == start) {
+				if (times == k) {
+					if (relation[i][1] == n - 1) {
+						plans++;
+					}
+				} else {
+					pass(relation[i][1], times + 1, n, relation, k);					
+				}
+			}
+    	}
+    }
 }
