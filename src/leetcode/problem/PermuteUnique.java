ce4e5eba6ce7354874a58f4954a28b518a5838cb
ELRNZgprc <richejohn@126.com>
2020-09-18 11:08:28 +0800

    添加 47. 全排列 II

--- src/leetcode/problem/PermuteUnique.java
+++ src/leetcode/problem/PermuteUnique.java
@@ -6,33 +6,35 @@ import java.util.List;
 
 public class PermuteUnique {
 	/**
-	 * 47. 全排列 II
+	 * 47. 全排列 II 
 	 * https://leetcode-cn.com/problems/permutations-ii/
 	 */
-	 public List<List<Integer>> permuteUnique(int[] nums) {
-	        List<List<Integer>> res = new ArrayList<>(); 
-	        if (nums == null || nums.length == 0) return res; 
-	        int[] visited = new int[nums.length]; 
-	        Arrays.sort(nums); 
-	        backTrack(res, nums, new ArrayList<Integer>(), visited);
-	        return res;
-	    }
+	public List<List<Integer>> permuteUnique(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		if (nums == null || nums.length == 0)
+			return res;
+		int[] visited = new int[nums.length];
+		Arrays.sort(nums);
+		backTrack(res, nums, new ArrayList<Integer>(), visited);
+		return res;
+	}
 
-	    private void backTrack(List<List<Integer>> res, int[] nums, List<Integer> list, int[] visited) {
-	        if (list.size() == nums.length) {
-	            res.add(new ArrayList<>(list));
-	            return ;
-	        }
-	        for (int i = 0; i < nums.length; i++) {
-	            if (visited[i] == 1) continue; 
-	            if (i > 0 && nums[i] == nums[i - 1] && visited[i - 1] == 0){
-	                continue;
-	            }
-	            list.add(nums[i]);
-	            visited[i] = 1;
-	            backTrack(res, nums, list, visited);
-	            visited[i] = 0;
-	            list.remove(list.size() - 1);
-	        }
-	    }
+	private void backTrack(List<List<Integer>> res, int[] nums, List<Integer> list, int[] visited) {
+		if (list.size() == nums.length) {
+			res.add(new ArrayList<>(list));
+			return;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (visited[i] == 1)
+				continue;
+			if (i > 0 && nums[i] == nums[i - 1] && visited[i - 1] == 0) {
+				continue;
+			}
+			list.add(nums[i]);
+			visited[i] = 1;
+			backTrack(res, nums, list, visited);
+			visited[i] = 0;
+			list.remove(list.size() - 1);
+		}
+	}
 }
