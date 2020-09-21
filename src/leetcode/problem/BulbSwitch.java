a8611f37c55a98862ad00c4116051f2755f96f01
ELRNZgprc <richejohn@126.com>
2020-09-21 09:25:59 +0800

    添加 319. 灯泡开关

---
+++ src/leetcode/problem/BulbSwitch.java
@@ -0,0 +1,11 @@
+package leetcode.problem;
+
+public class BulbSwitch {
+    /**
+     * 319. 灯泡开关
+     * https://leetcode-cn.com/problems/bulb-switcher/
+     */
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
