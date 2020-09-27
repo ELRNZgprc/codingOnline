dbff4b9bc8ab3bab142dc6b06e93b6750415c429
ELRNZgprc <richejohn@126.com>
2020-09-27 09:13:10 +0800

    添加 27. 移除元素

---
+++ src/leetcode/problem/RemoveElement.java
@@ -0,0 +1,20 @@
+package leetcode.problem;
+
+public class RemoveElement {
+    /**
+     * 27. 移除元素
+     * https://leetcode-cn.com/problems/remove-element/
+     */
+    public int removeElement(int[] nums, int val) {
+        if (nums.length < 1) {
+            return 0;
+        }
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[ans++] = nums[i];
+            }
+        }
+        return ans;
+    }
+}
