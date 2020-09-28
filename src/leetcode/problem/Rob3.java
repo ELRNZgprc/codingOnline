8ef17b40179880225d766d30961666d21c35b5f4
ELRNZgprc <richejohn@126.com>
2020-09-28 11:23:39 +0800

    添加 337. 打家劫舍 III

---
+++ src/leetcode/problem/Rob3.java
@@ -0,0 +1,23 @@
+package leetcode.problem;
+
+import leetcode.common.TreeNode;
+
+public class Rob3 {
+    /**
+     * 337. 打家劫舍 III
+     * https://leetcode-cn.com/problems/house-robber-iii/
+     */
+    public int rob(TreeNode root) {
+        return Math.max(work337(root, false), work337(root, true));
+    }
+
+    int work337(TreeNode root, boolean flag) {
+        if (root == null) {
+            return 0;
+        }
+        if (flag) {
+            return work337(root.left, false) + work337(root.right, false) + root.val;
+        }
+        return Math.max(work337(root.left, false), work337(root.left, true)) + Math.max(work337(root.right, false), work337(root.right, true));
+    }
+}
