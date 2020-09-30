d05e4344d5d80ae031d4a376e5772c90449f4c2b
ELRNZgprc <richejohn@126.com>
2020-09-30 08:51:58 +0800

    添加 701. 二叉搜索树中的插入操作

---
+++ src/leetcode/problem/InsertIntoBST.java
@@ -0,0 +1,21 @@
+package leetcode.problem;
+
+import leetcode.common.TreeNode;
+
+public class InsertIntoBST {
+	/**
+	 * 701. 二叉搜索树中的插入操作
+	 * https://leetcode-cn.com/problems/insert-into-a-binary-search-tree/
+	 */
+	public TreeNode insertIntoBST(TreeNode root, int val) {
+		if (root == null) {
+			return new TreeNode(val);
+		}
+		if (root.val < val) {
+			root.right = insertIntoBST(root.right, val);
+		} else {
+			root.left = insertIntoBST(root.left, val);
+		}
+		return root;
+	}
+}
