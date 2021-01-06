4d4cd20a27332dcecfab7b937c4d826ea72ebcb6
ELRNZgprc <richejohn@126.com>
2021-01-06 16:04:58 +0800

    添加 151. 翻转字符串里的单词

--- src/leetcode/problem/ReverseWords.java
+++ src/leetcode/problem/ReverseWords.java
@@ -1,17 +1,16 @@
 package leetcode.problem;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ReverseWords {
-    /**
-     * 557. 反转字符串中的单词 III
-     * https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/
-     */
-    public String reverseWords(String s) {
-        String[] strs = s.split(" ");
-        StringBuilder sb = new StringBuilder();
-        for (String str : strs) {
-            sb.append(new StringBuilder(str).reverse());
-            sb.append(" ");
-        }
-        return sb.toString().trim();
-    }
+	/**
+	 * 151. 翻转字符串里的单词
+	 * https://leetcode-cn.com/problems/reverse-words-in-a-string/
+	 */
+	public String reverseWords(String s) {
+		String[] words = s.trim().split(" +");
+		Collections.reverse(Arrays.asList(words));
+		return String.join(" ", words);
+	}
 }
