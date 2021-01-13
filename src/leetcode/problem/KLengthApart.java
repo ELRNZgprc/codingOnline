788f914684ad4e1ca0600e1ef78cdb2c7d871178
ELRNZgprc <richejohn@126.com>
2021-01-13 16:20:30 +0800

    添加 1437. 是否所有 1 都至少相隔 k 个元素

---
+++ src/leetcode/problem/KLengthApart.java
@@ -0,0 +1,19 @@
+package leetcode.problem;
+
+public class KLengthApart {
+	/**
+	 * 1437. 是否所有 1 都至少相隔 k 个元素
+	 * https://leetcode-cn.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+	 */
+	public boolean kLengthApart(int[] nums, int k) {
+		int before = Integer.MIN_VALUE;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 1) {
+				if (i - before - 1 < k)
+					return false;
+				before = i;
+			}
+		}
+		return true;
+	}
+}
