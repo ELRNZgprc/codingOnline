c1823b5e9d5bc1cd0adb07635c36dda56a3c18ab
ELRNZgprc <richejohn@126.com>
2021-03-11 08:55:20 +0800

    添加 186. 翻转字符串里的单词 II

--- src/leetcode/problem/ReverseWords.java
+++ src/leetcode/problem/ReverseWords.java
@@ -1,16 +1,30 @@
 package leetcode.problem;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 public class ReverseWords {
 	/**
-	 * 151. 翻转字符串里的单词
-	 * https://leetcode-cn.com/problems/reverse-words-in-a-string/
+	 * 186. 翻转字符串里的单词 II
+	 * https://leetcode-cn.com/problems/reverse-words-in-a-string-ii/
 	 */
-	public String reverseWords(String s) {
-		String[] words = s.trim().split(" +");
-		Collections.reverse(Arrays.asList(words));
-		return String.join(" ", words);
+	public void reverseWords(char[] s) {
+		final int N = s.length;
+		int start = 0;
+		for (int i = 0; i < N; i++) {
+			if (s[i] == ' ') {
+				reverse(s, start, i - 1);
+				start = i + 1;
+			}
+		}
+		reverse(s, start, N - 1);
+		reverse(s, 0, N - 1);
+	}
+
+	private void reverse(char[] chars, int start, int end) {
+		while (start < end) {
+			char temp = chars[start];
+			chars[start] = chars[end];
+			chars[end] = temp;
+			start++;
+			end--;
+		}
 	}
 }
