f5b3da714ef2558d2a57b414a86172065b5b936c
ELRNZgprc <richejohn@126.com>
2021-04-26 08:41:43 +0800

    添加 377. 组合总和 Ⅳ

---
+++ src/leetcode/problem/CombinationSum4.java
@@ -0,0 +1,23 @@
+package leetcode.problem;
+
+/**
+ * 377. 组合总和 Ⅳ
+ * https://leetcode-cn.com/problems/combination-sum-iv/
+ */
+public class CombinationSum4 {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp=new int[target+1];
+        dp[0]=1;
+        for(int i=1;i<=target;i++)
+        {
+            for(int num:nums)
+            {
+                if(i>=num)
+                {
+                    dp[i]+=dp[i-num];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
