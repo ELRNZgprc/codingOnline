0f1ab6b3a28b6b895ce1ca2519dc98719c019ea1
ELRNZgprc <richejohn@126.com>
2021-04-29 10:52:23 +0800

    添加 100. 相同的树

---
+++ src/leetcode/problem/IsSameTree.java
@@ -0,0 +1,22 @@
+package leetcode.problem;
+
+import leetcode.common.TreeNode;
+
+/**
+ * 100. 相同的树
+ * https://leetcode-cn.com/problems/same-tree/
+ */
+public class IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if ((p == null) && (q == null)) {
+            return true;
+        }
+        if ((p == null) || (q == null)) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
