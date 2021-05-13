5c06d92dafeacb1abfe047dc8b3267104ef4f87a
ELRNZgprc <richejohn@126.com>
2021-05-13 14:02:04 +0800

    修改 1737. 满足三条件之一需改变的最少字符数

---
+++ src/leetcode/problem/MinCharacters.java
@@ -0,0 +1,34 @@
+package leetcode.problem;
+
+/**
+ * 1737. 满足三条件之一需改变的最少字符数
+ * https://leetcode-cn.com/problems/change-minimum-characters-to-satisfy-one-of-three-conditions/
+ */
+public class MinCharacters {
+    public int minCharacters(String a, String b) {
+        int[] aHash = new int[26];
+        int[] bHash = new int[26];
+        for (char c : a.toCharArray()) {
+            aHash[c - 'a' + 1]++;
+        }
+        for (char c : b.toCharArray()) {
+            bHash[c - 'a' + 1]++;
+        }
+        int n = a.length();
+        int m = b.length();
+        int ans = n + m;
+        for (int i = 0; i < 26; i++) {
+            ans = Math.min(ans, n + m - aHash[i] - bHash[i]);
+        }
+
+        for (int i = 1; i < 26; i++) {
+            aHash[i] += aHash[i - 1];
+            bHash[i] += bHash[i - 1];
+        }
+        for (int i = 0; i < 25; i++) {
+            ans = Math.min(ans, aHash[i] + m - bHash[i]);
+            ans = Math.min(ans, n - aHash[i] + bHash[i]);
+        }
+        return ans;
+    }
+}
