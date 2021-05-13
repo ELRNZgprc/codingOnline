7432bf9efd907f03474fd091825ac4f9df49156b
ELRNZgprc <richejohn@126.com>
2021-05-13 09:04:34 +0800

    添加 1688. 比赛中的配对次数

---
+++ src/leetcode/problem/NumberOfMatches.java
@@ -0,0 +1,11 @@
+package leetcode.problem;
+
+/**
+ * 1688. 比赛中的配对次数
+ * https://leetcode-cn.com/problems/count-of-matches-in-tournament/
+ */
+public class NumberOfMatches {
+    public int numberOfMatches(int n) {
+        return n - 1;
+    }
+}
