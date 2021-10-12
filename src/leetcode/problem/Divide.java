b22594bcf7c86ff4946ae586e272582b4b6b2709
ELRNZgprc <richejohn@126.com>
2021-10-12 09:36:06 +0800

    添加 29. 两数相除

---
+++ src/leetcode/problem/Divide.java
@@ -0,0 +1,28 @@
+package leetcode.problem;
+
+/**
+ * 29. 两数相除
+ * https://leetcode-cn.com/problems/divide-two-integers/
+ */
+public class Divide {
+    public int divide(int dividend, int divisor) {
+        if (dividend == 0) {
+            return 0;
+        }
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        boolean negative;
+        negative = (dividend ^ divisor) < 0;
+        long t = Math.abs((long) dividend);
+        long d = Math.abs((long) divisor);
+        int result = 0;
+        for (int i = 31; i >= 0; i--) {
+            if ((t >> i) >= d) {
+                result += 1 << i;
+                t -= d << i;
+            }
+        }
+        return negative ? -result : result;
+    }
+}
