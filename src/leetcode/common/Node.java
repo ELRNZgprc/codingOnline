157caea7e8ddbfa006fec79e37596bbf9b5330bd
ELRNZgprc <richejohn@126.com>
2021-10-13 15:57:07 +0800

    修改 node

--- src/leetcode/common/Node.java
+++ src/leetcode/common/Node.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Node {
     public int val;
     public List<Node> neighbors;
+    public List<Node> children;
 
     public Node() {
         val = 0;
@@ -20,5 +21,10 @@ public class Node {
     public Node(int _val, ArrayList<Node> _neighbors) {
         val = _val;
         neighbors = _neighbors;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
     }
 }
