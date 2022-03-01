2eee9f0ee33fbf9bd1e1884cbce3657399e9887d
ELRNZgprc <richejohn@126.com>
2022-03-01 08:55:59 +0800

    添加 6. Z 字形变换

---
+++ src/leetcode/problem/Convert.java
@@ -0,0 +1,20 @@
+package leetcode.problem;
+
+/**
+ * 6. Z 字形变换
+ * https://leetcode-cn.com/problems/zigzag-conversion/
+ */
+public class Convert {
+    public String convert(String s, int numRows) {
+        String[] str = new String[numRows];
+        for (int i = 0; i < numRows; i++) str[i] = "";
+        int p = 0, n = s.length();
+        while (p < n) {
+            for (int i = 0; i < numRows && p < n; i++) str[i] += s.charAt(p++);
+            for (int i = numRows - 2; i >= 1 && p < n; i--) str[i] += s.charAt(p++);
+        }
+        String ans = "";
+        for (int i = 0; i < numRows; i++) ans += str[i];
+        return ans;
+    }
+}
