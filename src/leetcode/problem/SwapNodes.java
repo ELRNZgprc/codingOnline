14d66872bf4b768c45420f3ae1fbafdf868addf9
ELRNZgprc <richejohn@126.com>
2023-02-03 15:43:50 +0800

    添加 1721. 交换链表中的节点

--- src/leetcode/problem/SwapNodes.java
+++ src/leetcode/problem/SwapNodes.java
@@ -6,7 +6,7 @@ import leetcode.common.ListNode;
  * 1721. 交换链表中的节点
  * https://leetcode.cn/problems/swapping-nodes-in-a-linked-list/
  */
-public class swapNodes {
+public class SwapNodes {
     public ListNode swapNodes(ListNode head, int k) {
         ListNode cur = head;
         ListNode first = head;
