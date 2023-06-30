4592065b578c5ae75fb6d32cae2b1099dd51570c
ELRNZgprc <richejohn@126.com>
2023-06-30 11:11:34 +0800

    添加 583. 两个字符串的删除操作

---
+++ src/leetcode/problem/MinDistance.java
@@ -0,0 +1,25 @@
+package leetcode.problem;
+
+/**
+ * 583. 两个字符串的删除操作
+ * https://leetcode.cn/problems/delete-operation-for-two-strings/
+ */
+public class MinDistance {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return m + n - 2 * dp[m][n];
+    }
+}
